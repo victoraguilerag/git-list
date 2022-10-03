bdf4c987e20e4a856519e1d932491564f40bc3d0
Victor <victoraguilerag17@gmail.com>
2022-10-02 22:44:00 -0300

    Add API error handling

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,15 +2,23 @@ import { Commit } from "../models/commit.model";
 import { Repository } from "../models/repository.model";
 
 export const getRepos = async (username): Promise<Repository[]> => {
-  const queryParams = new URLSearchParams({ username });
-  const response = await fetch("/repos?" + queryParams.toString());
-  const results: Repository[] = await response.json();
-  return results;
+  try {
+    const queryParams = new URLSearchParams({ username });
+    const response = await fetch("/repos?" + queryParams.toString());
+    const results: Repository[] = await response.json();
+    return results;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const getCommits = async (repo, username): Promise<Commit[]> => {
-  const queryParams = new URLSearchParams({ repo, username });
-  const response = await fetch("/commits?" + queryParams.toString());
-  const results: Commit[] = await response.json();
-  return results;
+  try {
+    const queryParams = new URLSearchParams({ repo, username });
+    const response = await fetch("/commits?" + queryParams.toString());
+    const results: Commit[] = await response.json();
+    return results;
+  } catch (err) {
+    console.log(err);
+  }
 };
